b6b6454e531eb0a9d3ccb05e4a51f690e2266b7e
Servontius Turner <servontius.turner@me.com>
2024-01-18 21:53:32 -0600

    Completed Fib Sequence Code

--- src/main/java/org/servontiusturner/Main.java
+++ src/main/java/org/servontiusturner/Main.java
@@ -2,6 +2,19 @@ package org.servontiusturner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Fibonacci Sequence:");
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int currentNumber;
+
+        for (int counter = 1; counter <= 20; counter++) {
+            currentNumber = firstNumber + secondNumber;
+
+            System.out.println(currentNumber);
+
+            firstNumber = secondNumber;
+            secondNumber = currentNumber;
+        }
+
     }
 }
